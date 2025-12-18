c735fe3ad011b14128104b2dfdd154694e91a643
xgdgsc <xgdgsc@gmail.com>
2025-12-18 16:58:15 +0800

    🐛 fix hang

--- src/Streaming.jl
+++ src/Streaming.jl
@@ -99,6 +99,8 @@ function grpc_async_stream_response(
             handle_exception(req, ex; notify_ready=true)
             @error "grpc_async_stream_response: unexpected exception" exception = ex
             # rethrow(ex)
+        elseif isa(ex, InvalidStateException)
+            handle_exception(req, ex; notify_ready=true)
         end
     finally
         close(channel)
